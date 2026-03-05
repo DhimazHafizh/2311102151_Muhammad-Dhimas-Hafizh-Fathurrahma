dcf2b318297691e80e8f2f33cbaa2d76924d22cb
DhimazHafizh <dhmzz.hfzh@gmail.com>
2026-03-05 15:19:25 +0700

    tes

--- Week 2 - Modul 2/Unguided/unguided-no1/no1.go
+++ Week 2 - Modul 2/Unguided/unguided-no1/no1.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var tahun int
+	var Kabisat bool = false
+
+	fmt.Print("Masukkan tahun : ")
+	fmt.Scan(&tahun)
+
+	if tahun % 4 == 0 || tahun % 400 == 0 {
+		Kabisat = true
+	} else {
+		Kabisat = false
+	}
+
+	fmt.Println("Kabisat :", Kabisat)
 }
--- Week 2 - Modul 2/Unguided/unguided-no3/no3.go
+++ Week 2 - Modul 2/Unguided/unguided-no3/no3.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var beratGram int
+	var kg, sisaGramkg, sisaGram int
+	var biayakg, biayagram, totalBiaya int
+
+	fmt.Print("Masukkan total berat (gram): ")
+	fmt.Scan(&beratGram)
+
+	kg = beratGram / 1000
+	sisaGramkg = beratGram % 1000
+	sisaGram = sisaGramkg
+
+	biayakg = kg * 10000
+
+	if kg > 10 {
+		sisaGram = 0
+	} else {
+		if sisaGram >= 500 {
+			biayagram = sisaGram * 5
+		} else {
+			biayagram = sisaGram * 15
+		}
+	}
+	totalBiaya = biayakg + biayagram
+
+	fmt.Println()
+	fmt.Println("===== Detail Perhitungan =====")
+	fmt.Println("Detail berat : ", kg, " kg + ", sisaGramkg, " gram")
+	fmt.Println("Detail biaya  : Rp.", biayakg, " + Rp.", biayagram)
+	fmt.Println("Total biaya: Rp", totalBiaya)
 }
